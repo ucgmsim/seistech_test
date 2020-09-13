1519c2b57f98ddd54d44b6b748a9d3c923dfd8b2
Sung Eun Bae <sung.bae@canterbury.ac.nz>
2020-09-14 10:53:19 +1200

    Merged two tests into one

--- tests/test_computeHazardDisagg.py
+++ tests/test_computeHazardDisagg.py
@@ -14,7 +14,7 @@ from selenium.webdriver.chrome.options import Options
 chrome_options = Options()
 chrome_options.add_argument("--headless")
 
-class TestComputeHazardCurve():
+class TestComputeHazardDisagg():
   def setup_method(self, method):
     self.driver = webdriver.Chrome(options=chrome_options)
     self.vars = {}
@@ -22,7 +22,7 @@ class TestComputeHazardCurve():
   def teardown_method(self, method):
     self.driver.quit()
   
-  def test_computeHazardCurve(self):
+  def test_computeHazardDisagg(self):
     self.driver.get("https://development.seistech.nz/")
     self.driver.set_window_size(1680, 1027)
     self.driver.find_element(By.ID, "qs-login-btn").click()
@@ -32,16 +32,19 @@ class TestComputeHazardCurve():
     self.driver.find_element(By.NAME, "action").click()
     WebDriverWait(self.driver, 30000).until(expected_conditions.presence_of_element_located((By.XPATH, "//a[contains(text(),\'Hazard Analysis\')]")))
     self.driver.find_element(By.LINK_TEXT, "Hazard Analysis").click()
-#    element = self.driver.find_element(By.LINK_TEXT, "Hazard Analysis")
-#    actions = ActionChains(self.driver)
-#    actions.move_to_element(element).perform()
+    element = self.driver.find_element(By.LINK_TEXT, "Hazard Analysis")
+    actions = ActionChains(self.driver)
+    actions.move_to_element(element).perform()
     self.driver.execute_script("window.scrollTo(0,0)")
-#    element = self.driver.find_element(By.CSS_SELECTOR, "body")
-#    actions = ActionChains(self.driver)
-#    actions.move_to_element(element, 0, 0).perform()
+    self.driver.find_element(By.ID, "haz-lat").click()
+    self.driver.find_element(By.ID, "haz-lat").clear()
+    self.driver.find_element(By.ID, "haz-lat").send_keys("-43.60")
+    self.driver.find_element(By.ID, "haz-lng").click()
+    self.driver.find_element(By.ID, "haz-lng").clear()
+    self.driver.find_element(By.ID, "haz-lng").send_keys("172.72")
     self.driver.find_element(By.ID, "site-selection").click()
     time.sleep(3)
-    WebDriverWait(self.driver, 10000).until(expected_conditions.invisibility_of_element_located((By.CSS_SELECTOR, "#uncontrolled-tab-example-tab-hazard[aria-disabled]")))
+    WebDriverWait(self.driver, 20000).until(expected_conditions.invisibility_of_element_located((By.CSS_SELECTOR, "#uncontrolled-tab-example-tab-hazard[aria-disabled]")))
     self.driver.find_element(By.XPATH, "//a[contains(.,\'Seismic Hazard\')]").click()
     self.driver.find_element(By.ID, "IMs").click()
     dropdown = self.driver.find_element(By.ID, "IMs")
@@ -50,5 +53,13 @@ class TestComputeHazardCurve():
     self.driver.find_element(By.ID, "im-select").click()
     WebDriverWait(self.driver, 30000).until(expected_conditions.invisibility_of_element_located((By.CSS_SELECTOR, ".hazard-curve-viewer > .download-button[disabled]")))
     self.driver.find_element(By.CSS_SELECTOR, ".hazard-curve-viewer > .download-button").click()
+    self.driver.find_element(By.ID, "im-select").click()
+    self.driver.find_element(By.ID, "annual-rate").click()
+    self.driver.find_element(By.ID, "annual-rate").clear()
+    self.driver.find_element(By.ID, "annual-rate").send_keys("0.2")
+    self.driver.find_element(By.ID, "prob-update").click()
+    self.driver.find_element(By.XPATH, "//a[contains(text(),\'Disaggregation\')]").click()
+    WebDriverWait(self.driver, 60000).until(expected_conditions.invisibility_of_element_located((By.CSS_SELECTOR, "#uncontrolled-tab-example-tabpane-disagg > .download-button[disabled]")))
+    self.driver.find_element(By.CSS_SELECTOR, "#uncontrolled-tab-example-tabpane-disagg > .download-button").click()
     time.sleep(10)
   
