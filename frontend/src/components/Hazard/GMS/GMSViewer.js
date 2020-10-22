0b170710fa4842ac435b2c83f45389b542857cd6
sdw123123@gmail.com <sdw123123@gmail.com>
2020-10-22 16:55:17 +1300

    Adding Warning and Guide mssages

--- frontend/src/components/Hazard/GMS/GMSViewer.js
+++ frontend/src/components/Hazard/GMS/GMSViewer.js
@@ -3,11 +3,14 @@ import Tabs from "react-bootstrap/Tabs";
 import { Tab } from "react-bootstrap";
 import Select from "react-select";
 import { GlobalContext } from "context";
+import * as CONSTANTS from "constants/Constants";
 
 import FirstPlot from "./FirstPlot";
 import SecondPlot from "./SecondPlot";
-import ThirdPlot from "./ThirdPlot"
+import ThirdPlot from "./ThirdPlot";
 import LoadingSpinner from "components/common/LoadingSpinner";
+import GuideMessage from "components/common/GuideMessage";
+import ErrorMessage from "components/common/ErrorMessage";
 
 import "assets/style/GMSViewer.css";
 
@@ -28,48 +31,106 @@ const GMSViewer = () => {
     }));
     setLocalIMVectors(localIMs);
 
-    let localPeriods = {}
+    // Set the first IM as a default IM for plot
+    setSpecifiedIM(localIMs[0]);
+
+    // Create an object key = IM, value = Period
+    let localPeriods = {};
     selectedIMVectors.forEach((IM) => {
       localPeriods[IM] = IM.split("_")[1];
-    })
+    });
     setPeriods(localPeriods);
   }, [selectedIMVectors]);
+
+  const validateComputedGMS = () => {
+    let isValidResponse = true;
+
+    Object.values(computedGMS).forEach((x) => {
+      if (Object.keys(x).length === 0) {
+        isValidResponse = false;
+      }
+    });
+
+    return isValidResponse;
+  };
 
   return (
     <div className="gms-viewer">
       <Tabs defaultActiveKey="firstPlot">
         <Tab eventKey="firstPlot" title="Specific IM">
+          {isLoading === false && computedGMS === null && (
+            <GuideMessage
+              header={CONSTANTS.GMS}
+              body={CONSTANTS.GMS_VIEWER_GUIDE_MSG}
+              instruction={CONSTANTS.GMS_VIEWER_GUIDE_INSTRUCTION}
+            />
+          )}
           {isLoading === true && <LoadingSpinner />}
           {isLoading === false && computedGMS !== null && (
             <Fragment>
-              <Select
-                id="im-vectors"
-                onChange={(value) => setSpecifiedIM(value || [])}
-                defaultValue={specifiedIM}
-                options={localIMVectors}
-              />
-              <FirstPlot gmsData={computedGMS} IM={specifiedIM.value} />
+              {validateComputedGMS() === false ? (
+                <ErrorMessage />
+              ) : (
+                <Fragment>
+                  <Select
+                    id="im-vectors"
+                    onChange={(value) => setSpecifiedIM(value || [])}
+                    defaultValue={specifiedIM}
+                    options={localIMVectors}
+                  />
+                  <FirstPlot gmsData={computedGMS} IM={specifiedIM.value} />
+                </Fragment>
+              )}
             </Fragment>
           )}
         </Tab>
         <Tab eventKey="secondPlot" title="Second Plot">
+          {isLoading === false && computedGMS === null && (
+            <GuideMessage
+              header={CONSTANTS.GMS}
+              body={CONSTANTS.GMS_VIEWER_GUIDE_MSG}
+              instruction={CONSTANTS.GMS_VIEWER_GUIDE_INSTRUCTION}
+            />
+          )}
           {isLoading === true && <LoadingSpinner />}
           {isLoading === false && computedGMS !== null && (
             <Fragment>
-              <SecondPlot gmsData={computedGMS} periods={periods} />
+              {validateComputedGMS() === false ? (
+                <ErrorMessage />
+              ) : (
+                <SecondPlot gmsData={computedGMS} periods={periods} />
+              )}
             </Fragment>
           )}
         </Tab>
         <Tab eventKey="thirdPlot" title="Third Plot">
-        {isLoading === true && <LoadingSpinner />}
+          {isLoading === false && computedGMS === null && (
+            <GuideMessage
+              header={CONSTANTS.GMS}
+              body={CONSTANTS.GMS_VIEWER_GUIDE_MSG}
+              instruction={CONSTANTS.GMS_VIEWER_GUIDE_INSTRUCTION}
+            />
+          )}
+          {isLoading === true && <LoadingSpinner />}
           {isLoading === false && computedGMS !== null && (
             <Fragment>
-              <ThirdPlot gmsData={computedGMS} />
+              {validateComputedGMS() === false ? (
+                <ErrorMessage />
+              ) : (
+                <ThirdPlot gmsData={computedGMS} />
+              )}
             </Fragment>
           )}
         </Tab>
         <Tab eventKey="fourthPlot" title="Fourth Plot">
-          <div>HELLO</div>
+          {isLoading === false && computedGMS === null && (
+            <GuideMessage
+              header={CONSTANTS.GMS}
+              body={CONSTANTS.GMS_VIEWER_GUIDE_MSG}
+              instruction={CONSTANTS.GMS_VIEWER_GUIDE_INSTRUCTION}
+            />
+          )}
+          {isLoading === true && <LoadingSpinner />}
         </Tab>
       </Tabs>
     </div>
