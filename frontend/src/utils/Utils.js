a2c404bcb8e676a37fe14658bc4d7aff6a42211e
sdw123123@gmail.com <sdw123123@gmail.com>
2020-11-02 16:41:42 +1300

    implement qcore's IM sort

--- frontend/src/utils/Utils.js
+++ frontend/src/utils/Utils.js
@@ -56,3 +56,59 @@ export const range = (start, stop, step = 1) =>
   Array(Math.ceil((stop - start) / step) + 1)
     .fill(start)
     .map((x, y) => x + y * step);
+
+/*
+  JS version of qcore IM Sort
+*/
+
+const DEFAULT_PATTERN_ORDER = [
+  "station",
+  "component",
+  "PGA",
+  "PGV",
+  "CAV",
+  "AI",
+  "Ds575",
+  "Ds595",
+  "Ds2080",
+  "MMI",
+  "pSA",
+  "FAS",
+  "IESDR",
+];
+
+export const orderIMs = (unsortedIMs) => {
+  const adjIMs = [];
+
+  if (unsortedIMs.length !== 0) {
+    for (const pattern of DEFAULT_PATTERN_ORDER) {
+      const curIMs = Array.from(unsortedIMs, (x) => {
+        if (x.startsWith(pattern) === true) {
+          return x;
+        }
+      });
+
+      if (curIMs.length === 0) {
+        continue;
+      } else if (curIMs.length === 1) {
+        adjIMs.push(curIMs[0]);
+      } else {
+        const tempSortedIMs = curIMs.sort((a, b) => {
+          return a.localeCompare(b, undefined, {
+            numeric: true,
+            sensitivity: "base",
+          });
+        });
+        tempSortedIMs.forEach((x) => {
+          adjIMs.push(x);
+        });
+      }
+    }
+  }
+
+  const filteredAdjIms = adjIMs.filter((element) => {
+    return element !== undefined;
+  });
+
+  return filteredAdjIms;
+};
