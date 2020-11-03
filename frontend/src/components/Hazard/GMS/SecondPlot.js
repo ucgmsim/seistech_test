3f65107debc51016ff9ac5ba567965ad73ef06ab
sdw123123@gmail.com <sdw123123@gmail.com>
2020-11-04 12:19:42 +1300

    Sort IM in frontend, instead of doing backend to do so.

--- frontend/src/components/Hazard/GMS/SecondPlot.js
+++ frontend/src/components/Hazard/GMS/SecondPlot.js
@@ -13,7 +13,19 @@ const SecondPlot = ({ gmsData, periods }) => {
     const realisations = gmsData["realisations"];
     const selectedGMs = gmsData["selected_GMs"];
 
+    const periodsArray = Object.values(periods);
     const scatterArr = [];
+    const sortedCDFX = {};
+    const sortedCDFY = {};
+    const sortedRealisations = {};
+    const sortedSelectedGMs = {};
+
+    for (const [IM, values] of Object.entries(periods)) {
+      sortedCDFX[IM] = cdfX[IM];
+      sortedCDFY[IM] = cdfY[IM];
+      sortedRealisations[IM] = realisations[IM];
+      sortedSelectedGMs[IM] = selectedGMs[IM];
+    }
 
     /*
       GCIM calculations
@@ -23,7 +35,7 @@ const SecondPlot = ({ gmsData, periods }) => {
     const higherPercenIndexObj = {};
 
     // We are going to compare Y value to 0.5 (Median), 0.16 (16th percentile) and 0.84 (84th percentile)
-    for (const [IM, values] of Object.entries(cdfY)) {
+    for (const [IM, values] of Object.entries(sortedCDFY)) {
       // Median
       const medianFound = values.find((element) => element >= 0.5);
       medianIndexObj[IM] = values.indexOf(medianFound);
@@ -41,7 +53,7 @@ const SecondPlot = ({ gmsData, periods }) => {
     const medianValues = [];
     const lowerPercenValues = [];
 
-    for (const [IM, values] of Object.entries(cdfX)) {
+    for (const [IM, values] of Object.entries(sortedCDFX)) {
       upperPercenValues.push(values[higherPercenIndexObj[IM]]);
       medianValues.push(values[medianIndexObj[IM]]);
       lowerPercenValues.push(values[lowerPercenIndexObj[IM]]);
@@ -49,7 +61,7 @@ const SecondPlot = ({ gmsData, periods }) => {
 
     scatterArr.push(
       {
-        x: Object.values(periods),
+        x: periodsArray,
         y: upperPercenValues,
         mode: "lines",
         name: "GCIM - 84th Percentile",
@@ -57,7 +69,7 @@ const SecondPlot = ({ gmsData, periods }) => {
         type: "scatter",
       },
       {
-        x: Object.values(periods),
+        x: periodsArray,
         y: medianValues,
         mode: "lines",
         name: "GCIM - Median",
@@ -65,7 +77,7 @@ const SecondPlot = ({ gmsData, periods }) => {
         type: "scatter",
       },
       {
-        x: Object.values(periods),
+        x: periodsArray,
         y: lowerPercenValues,
         mode: "lines",
         name: "GCIM - 16th percentile",
@@ -77,13 +89,13 @@ const SecondPlot = ({ gmsData, periods }) => {
     /*
       Realisations calculation
     */
-    for (let i = 0; i < Object.values(realisations)[0].length; i++) {
+    for (let i = 0; i < Object.values(sortedRealisations)[0].length; i++) {
       let yCoords = [];
-      for (const [IM, period] of Object.entries(realisations)) {
-        yCoords.push(realisations[IM][i]);
+      for (const [IM, period] of Object.entries(sortedRealisations)) {
+        yCoords.push(sortedRealisations[IM][i]);
       }
       scatterArr.push({
-        x: Object.values(periods),
+        x: periodsArray,
         y: yCoords,
         legendgroup: "Realisations",
         mode: "lines",
@@ -96,13 +108,13 @@ const SecondPlot = ({ gmsData, periods }) => {
     /*
       Selected GMs calculation
     */
-    for (let i = 0; i < Object.values(selectedGMs)[0].length; i++) {
+    for (let i = 0; i < Object.values(sortedSelectedGMs)[0].length; i++) {
       let yCoords = [];
-      for (const [IM, period] of Object.entries(selectedGMs)) {
-        yCoords.push(selectedGMs[IM][i]);
+      for (const [IM, period] of Object.entries(sortedSelectedGMs)) {
+        yCoords.push(sortedSelectedGMs[IM][i]);
       }
       scatterArr.push({
-        x: Object.values(periods),
+        x: periodsArray,
         y: yCoords,
         legendgroup: "Selected GMs",
         mode: "lines",
