7e8f805c1f54679b23b491bd5f279b2e2e507c7c
sdw123123@gmail.com <sdw123123@gmail.com>
2020-11-05 11:26:17 +1300

    Update IMSelect component to have param called options to use

--- frontend/src/components/common/IMSelect.js
+++ frontend/src/components/common/IMSelect.js
@@ -1,31 +1,26 @@
-import React, { useContext, Fragment, useEffect, useState } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import Select from "react-select";
 
-import { GlobalContext } from "context";
-
-const IMSelect = ({ title, setIM }) => {
-  const { IMs, setIMVectors } = useContext(GlobalContext);
-
+const IMSelect = ({ title, setIM, options }) => {
   const [localIMs, setLocalIMs] = useState([]);
 
   useEffect(() => {
-    let localIMs = IMs.map((IM) => ({
+    let localIMs = options.map((IM) => ({
       value: IM,
       label: IM,
     }));
     setLocalIMs(localIMs);
-    setIMVectors(localIMs);
-  }, [IMs]);
+  }, [options]);
 
   return (
     <Fragment>
       <pre>{title}</pre>
       <Select
         id="IMs"
-        placeholder={IMs.length === 0 ? "Loading..." : "Select..."}
+        placeholder={options.length === 0 ? "Loading..." : "Select..."}
         onChange={(e) => setIM(e.value)}
         options={localIMs}
-        isDisabled={IMs.length === 0}
+        isDisabled={options.length === 0}
       />
     </Fragment>
   );
