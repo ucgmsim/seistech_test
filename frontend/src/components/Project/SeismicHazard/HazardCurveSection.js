0ee453759589156298792a8d23e845bd645ae95b
sdw123123@gmail.com <sdw123123@gmail.com>
2020-11-23 15:35:59 +1300

    scaffolded Hazard Curve for project

---
+++ frontend/src/components/Project/SeismicHazard/HazardCurveSection.js
@@ -0,0 +1,60 @@
+import React, { useState, useContext, useEffect, Fragment } from "react";
+import Select from "react-select";
+import { v4 as uuidv4 } from "uuid";
+import { GlobalContext } from "context";
+
+const HazardCurveSection = () => {
+  const { projectSelectedIM, setProjectSelectedIM } = useContext(GlobalContext);
+
+  const options = [
+    {
+      value: "A Test",
+      label: "A Test",
+    },
+    {
+      value: "B Test",
+      label: "B Test",
+    },
+    {
+      value: "C Test",
+      label: "C Test",
+    },
+  ];
+
+  const displayInConsole = () => {
+    console.log(`Im chosen IM: ${projectSelectedIM}`);
+  };
+
+  return (
+    <Fragment>
+      <div className="form-group form-section-title">
+        <span>Hazard Curve</span>
+      </div>
+      <div className="form-group">
+        <Select
+          id="intensity-measure-id"
+          placeholder={options.length === 0 ? "Loading..." : "Select..."}
+          onChange={(e) => setProjectSelectedIM(e.value)}
+          options={options}
+          isDisabled={options.length === 0}
+        />
+      </div>
+
+      <div className="form-group">
+        <button
+          id="project-hazard-curve-get-btn"
+          type="button"
+          className="btn btn-primary"
+          disabled={projectSelectedIM === null}
+          onClick={() => {
+            displayInConsole();
+          }}
+        >
+          Get
+        </button>
+      </div>
+    </Fragment>
+  );
+};
+
+export default HazardCurveSection;
