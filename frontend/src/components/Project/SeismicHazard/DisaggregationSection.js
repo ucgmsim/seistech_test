95b6cc3f3a4f81320326da50709aa14d0cb62a4c
sdw123123@gmail.com <sdw123123@gmail.com>
2020-11-23 15:38:00 +1300

    scaffolded Disaggregation for Project

---
+++ frontend/src/components/Project/SeismicHazard/DisaggregationSection.js
@@ -0,0 +1,66 @@
+import React, { useState, useContext, useEffect, Fragment } from "react";
+import { v4 as uuidv4 } from "uuid";
+import { GlobalContext } from "context";
+import * as CONSTANTS from "constants/Constants";
+import { disableScrollOnNumInput } from "utils/Utils";
+import TextField from "@material-ui/core/TextField";
+import Select from "react-select";
+
+const HazardCurveSection = () => {
+  const [returnPeriod, setReturnPeriod] = useState(null);
+  const { projectSelectedIM } = useContext(GlobalContext);
+
+  const options = [
+    {
+      value: "A Test",
+      label: "A Test",
+    },
+    {
+      value: "B Test",
+      label: "B Test",
+    },
+    {
+      value: "C Test",
+      label: "C Test",
+    },
+  ];
+
+  const displayInConsole = () => {
+    console.log(
+      `Im Disaggregation Return Period: ${returnPeriod} and selected IM ${projectSelectedIM}`
+    );
+  };
+
+  return (
+    <Fragment>
+      <div className="form-group form-section-title">
+        <span>Disaggregation</span>
+      </div>
+      <div className="form-group">
+        <Select
+          id="return-period-id"
+          placeholder={options.length === 0 ? "Loading..." : "Select..."}
+          onChange={(e) => setReturnPeriod(e.value)}
+          options={options}
+          isDisabled={options.length === 0 || projectSelectedIM === null}
+        />
+      </div>
+
+      <div className="form-group">
+        <button
+          id="project-hazard-curve-get-btn"
+          type="button"
+          className="btn btn-primary"
+          disabled={returnPeriod === null}
+          onClick={() => {
+            displayInConsole();
+          }}
+        >
+          Get
+        </button>
+      </div>
+    </Fragment>
+  );
+};
+
+export default HazardCurveSection;
