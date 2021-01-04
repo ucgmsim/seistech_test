f595a6063782b1320278026ff9314aaf5b58d1f2
sdw123123@gmail.com <sdw123123@gmail.com>
2021-01-05 11:46:08 +1300

    Update the user_id to be string and its from Auth0's user_id which is unique and time to NZ time.

--- middleware/middleware/models.py
+++ middleware/middleware/models.py
@@ -1,11 +1,12 @@
 from datetime import datetime
+import pytz
 from flask_sqlalchemy import SQLAlchemy
 from server import db
 
 # Bridging table between Project and User table
 available_projects_table = db.Table(
     "available_projects",
-    db.Column("user_id", db.Integer, db.ForeignKey("user.user_id")),
+    db.Column("user_id", db.String(50), db.ForeignKey("user.user_id")),
     db.Column("project_id", db.Integer, db.ForeignKey("project.project_id")),
 )
 
@@ -19,8 +20,7 @@ class Project(db.Model):
 
 
 class User(db.Model):
-    user_id = db.Column(db.Integer, primary_key=True)
-    user_name = db.Column(db.String(50), unique=True)
+    user_id = db.Column(db.String(50), primary_key=True)
     histories = db.relationship("History", backref="owner")
     # To create a connection between Project and available_projects table
     # By using lazy attribute with dynamic, instead of getting all the data to view, get a query to filter out
@@ -30,15 +30,17 @@ class User(db.Model):
         backref=db.backref("allocate", lazy="dynamic"),
     )
 
-    def __init__(self, name):
-        self.user_name = name
+    def __init__(self, user_id):
+        self.user_id = user_id
 
 
 class History(db.Model):
     history_id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey("user.user_id"))
+    user_id = db.Column(db.String(50), db.ForeignKey("user.user_id"))
     endpoint = db.Column(db.String(30))
-    date = db.Column(db.DateTime, default=datetime.utcnow)
+    date = db.Column(
+        db.DateTime, default=datetime.now(pytz.timezone("Pacific/Auckland"))
+    )
     history_requests = db.relationship("History_Request", backref="record")
 
     def __init__(self, user_id, endpoint):
