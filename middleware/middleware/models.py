eef431fd841a52ddfab2513d26268460d7841059
sdw123123@gmail.com <sdw123123@gmail.com>
2021-01-06 11:18:15 +1300

    Increase the string(VARCHAR) length to 100

--- middleware/middleware/models.py
+++ middleware/middleware/models.py
@@ -6,21 +6,21 @@ from server import db
 # Bridging table between Project and User table
 available_projects_table = db.Table(
     "available_projects",
-    db.Column("user_id", db.String(50), db.ForeignKey("user.user_id")),
+    db.Column("user_id", db.String(100), db.ForeignKey("user.user_id")),
     db.Column("project_id", db.Integer, db.ForeignKey("project.project_id")),
 )
 
 
 class Project(db.Model):
     project_id = db.Column(db.Integer, primary_key=True)
-    project_name = db.Column(db.String(50))
+    project_name = db.Column(db.String(100))
 
     def __init__(self, name):
         self.project_name = name
 
 
 class User(db.Model):
-    user_id = db.Column(db.String(50), primary_key=True)
+    user_id = db.Column(db.String(100), primary_key=True)
     histories = db.relationship("History", backref="owner")
     # To create a connection between Project and available_projects table
     # By using lazy attribute with dynamic, instead of getting all the data to view, get a query to filter out
@@ -36,7 +36,7 @@ class User(db.Model):
 
 class History(db.Model):
     history_id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.String(50), db.ForeignKey("user.user_id"))
+    user_id = db.Column(db.String(100), db.ForeignKey("user.user_id"))
     endpoint = db.Column(db.String(100))
     date = db.Column(
         db.DateTime, default=datetime.now(pytz.timezone("Pacific/Auckland"))
@@ -51,8 +51,8 @@ class History(db.Model):
 class History_Request(db.Model):
     history_request_id = db.Column(db.Integer, primary_key=True)
     history_id = db.Column(db.Integer, db.ForeignKey("history.history_id"))
-    attribute = db.Column(db.String(30))
-    value = db.Column(db.String(50))
+    attribute = db.Column(db.String(100))
+    value = db.Column(db.String(100))
 
     def __init__(self, history_id, attribute, value):
         self.history_id = history_id
