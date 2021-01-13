fc24c9de2596f49eb7ffa34484ce70672b65e631
sdw123123@gmail.com <sdw123123@gmail.com>
2021-01-14 11:19:51 +1300

    Update based on the feedback

--- middleware/middleware/add_exist_users.py
+++ middleware/middleware/add_exist_users.py
@@ -2,7 +2,6 @@ import os
 import json
 import http.client
 import requests
-from jose import jwt
 from models import *
 
 # To communicate with Management API
@@ -39,7 +38,6 @@ def get_management_api_token():
 
 def get_users():
     """Get all users"""
-
     resp = requests.get(
         AUTH0_AUDIENCE + "users",
         headers={"Authorization": "Bearer {}".format(get_management_api_token())},
@@ -51,17 +49,17 @@ def get_users():
     user_id_list = []
 
     # We want to store an actual id that comes after auth| or google| so split string by |
-    for dic in user_list:
-        for key in dic:
-            if key == "user_id":
-                user_id_list.append(dic[key].split("|")[1])
+    for user_dic in user_list:
+        if "user_id" in user_dic.keys():
+            user_id_list.append(user_dic["user_id"].split("|")[1])
+        else:
+            print(f"WARNING: No user_id found for user_dict {user_dict}")
 
     return user_id_list
 
 
 def insert_all_users():
     """Insert all users into MariaDB. Need to run only once"""
-
     users = get_users()
 
     for user in users:
