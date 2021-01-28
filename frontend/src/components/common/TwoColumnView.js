fb5169594d28082fb03807e26a3cb0b2e5b0bbc9
sdw123123@gmail.com <sdw123123@gmail.com>
2021-01-28 14:57:54 +1300

    Managed to create an exact same behaviour two columnview but more flexible (e.g. mobile view)

--- frontend/src/components/common/TwoColumnView.js
+++ frontend/src/components/common/TwoColumnView.js
@@ -8,21 +8,6 @@ class TwoColumnView extends React.Component {
 
     return (
       <Fragment>
-        {/* <div className="hazard-inner">
-          <div className="row two-column-row">
-            <div className="col-3 controlGroup form-panel">
-              <div>
-                <Left />
-              </div>
-            </div>
-            <div className="col-9 controlGroup form-viewer">
-              <div className="two-column-view-right-pane">
-                <Right />
-              </div>
-            </div>
-          </div>
-        </div> */}
-
         <div className="container">
           <div className="row">
             <div className="col-lg-3 control-group">
