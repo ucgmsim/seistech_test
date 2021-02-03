682d6b422e3f95b6b19eeccd3df24b7a98be2b6e
sdw123123@gmail.com <sdw123123@gmail.com>
2021-02-03 13:53:49 +1300

    Replace to material-ul's Tooltip to achieve requirements.

--- frontend/src/components/common/GuideTooltip.js
+++ frontend/src/components/common/GuideTooltip.js
@@ -1,28 +1,46 @@
-import React, { Fragment } from "react";
-
-import { Tooltip, OverlayTrigger } from "react-bootstrap";
+import React, { Fragment, useState } from "react";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import Tooltip from "@material-ui/core/Tooltip";
+import ClickAwayListener from "@material-ui/core/ClickAwayListener";
 
 import "assets/style/GuideTooltip.css";
 
 const GuideTooltip = ({ explanation }) => {
-  const renderTooltip = (props) => {
-    return (
-      <Tooltip id="guide-tooltip" {...props}>
-        {explanation}
-      </Tooltip>
-    );
-  };
+  const [open, setOpen] = useState(false);
+
+  const handleTooltipClose = () => setOpen(false);
+
+  const handleTooltipOpen = () => setOpen(true);
+
   return (
     <Fragment>
-      <OverlayTrigger
-        placement="right"
-        delay={{ show: 250, hide: 400 }}
-        overlay={renderTooltip}
-      >
-        <FontAwesomeIcon icon="question-circle" size="sm" className="ml-2" />
-      </OverlayTrigger>
+      <ClickAwayListener onClickAway={handleTooltipClose}>
+        <Tooltip
+          PopperProps={{
+            disablePortal: false,
+          }}
+          onClose={handleTooltipClose}
+          open={open}
+          disableFocusListener
+          disableHoverListener
+          disableTouchListener
+          placement="right"
+          interactive
+          title={explanation}
+          // title={<a href="https://naver.com">VISIT here!</a>}
+          arrow
+        >
+          <span onClick={handleTooltipOpen}>
+            <FontAwesomeIcon
+              icon="question-circle"
+              size="sm"
+              className="ml-2"
+            />
+          </span>
+        </Tooltip>
+      </ClickAwayListener>
     </Fragment>
   );
 };
