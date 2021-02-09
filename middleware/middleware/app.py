4b5add6bb6d31d94fa971ea2b527b87ec3afdc03
sdw123123@gmail.com <sdw123123@gmail.com>
2021-02-10 10:37:19 +1300

    Can now get user infro from Auth0 and also Project from Project API

--- middleware/middleware/app.py
+++ middleware/middleware/app.py
@@ -1,6 +1,7 @@
 import os
 
 from middleware import app
+from pyasn1 import debug
 
 if __name__ == "__main__":
     app.run(
@@ -8,4 +9,5 @@ if __name__ == "__main__":
         host="0.0.0.0",
         processes=int(os.environ["N_PROCS"]),
         port=int(os.environ["INTER_PORT"]),
+        debug=True,
     )
