7554174a3b6261cf08740239204ca128ac2929e6
Tom son <sdw123123@gmail.com>
2021-02-23 16:55:26 +1300

    Convert to a functional component

--- frontend/src/components/common/TwoColumnView.js
+++ frontend/src/components/common/TwoColumnView.js
@@ -1,26 +1,26 @@
 import React from "react";
 
-// Under Hazard Analysis, it splits screen to left and right
-class TwoColumnView extends React.Component {
-  render() {
-    const Left = this.props.cpanel;
-    const Right = this.props.viewer;
+import "assets/style/TwoColumnView.css";
 
-    return (
-      <div className="hazard-inner">
-        <div className="row two-column-row">
-          <div className="col-3 controlGroup form-panel">
-            <Left />
-          </div>
-          <div className="col-9 controlGroup form-viewer">
-            <div className="two-column-view-right-pane">
-              <Right />
-            </div>
+// Under Hazard Analysis, it splits screen to left and right
+const TwoColumnView = ({ cpanel, viewer }) => {
+  const Left = cpanel;
+  const Right = viewer;
+
+  return (
+    <div className="two-column-inner">
+      <div className="row two-column-row">
+        <div className="col-3 controlGroup form-panel">
+          <Left />
+        </div>
+        <div className="col-9 controlGroup form-viewer">
+          <div className="two-column-view-right-pane">
+            <Right />
           </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default TwoColumnView;
