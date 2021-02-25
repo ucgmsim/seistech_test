9fa2cc1be72accdc4a6b56119d8593f56e8c065a
sdw123123@gmail.com <sdw123123@gmail.com>
2021-02-25 14:54:24 +1300

    Adopt Jason's feedback to move a script file to a script directory

--- middleware/middleware/script/create_db.py
+++ middleware/middleware/script/create_db.py
@@ -2,8 +2,8 @@ import os
 
 from flask import Flask
 
-from custom_sqlalchemy import CustomSQLALchemy
-from auth0 import get_users
+from ..custom_sqlalchemy import CustomSQLALchemy
+from ..auth0 import get_users
 
 
 # DB Connection Setup
@@ -22,7 +22,7 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = CustomSQLALchemy(app)
 
-from models import *
+from ..models import *
 
 # Create tables - It only creates when tables don't exist
 db.create_all()
@@ -30,7 +30,25 @@ db.session.commit()
 
 print(get_users())
 
-# Adding all users from Auth0 to the DB
+# Need to be manually done as we cannot pull permission data from Auth0
+PERMISSION_LIST = [
+    "create-project",
+    "edit-user",
+    "hazard",
+    "hazard:hazard",
+    "hazard:disagg",
+    "hazard:uhs",
+    "hazard:gms",
+    "project",
+    "psha-admin",
+]
+
+# Adding all users from Auth0 to the User table
 for key in get_users():
     db.session.add(User(key))
+    db.session.commit()
+
+# Adding all permission to the PageAccessPermission table
+for permission in PERMISSION_LIST:
+    db.session.add(PageAccessPermission(permission))
     db.session.commit()
