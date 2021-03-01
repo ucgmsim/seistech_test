54a85dda1fa82d0c210e9ec34d9d4e7e05bf7695
sdw123123@gmail.com <sdw123123@gmail.com>
2021-03-01 15:29:04 +1300

    Rename a few tables

--- middleware/middleware/models.py
+++ middleware/middleware/models.py
@@ -2,8 +2,8 @@ from datetime import datetime
 from server import db
 
 
-class AvailableProject(db.Model):
-    __tablename__ = "available_project"
+class AllowedProject(db.Model):
+    __tablename__ = "allowed_project"
     user_id = db.Column(
         "user_id", db.String(100), db.ForeignKey("user.user_id"), primary_key=True
     )
@@ -23,7 +23,7 @@ class Project(db.Model):
     project_id = db.Column(db.Integer, primary_key=True)
     project_name = db.Column(db.String(100))
 
-    users = db.relationship("AvailableProject", back_populates="project")
+    users = db.relationship("AllowedProject", back_populates="project")
 
     def __init__(self, name):
         self.project_name = name
@@ -32,8 +32,8 @@ class Project(db.Model):
         return "<Project %r>" % self.project_name
 
 
-class GrantedPermission(db.Model):
-    __tablename__ = "granted_permission"
+class AllowedPermission(db.Model):
+    __tablename__ = "allowed_permission"
     user_id = db.Column(
         "user_id", db.String(100), db.ForeignKey("user.user_id"), primary_key=True
     )
@@ -56,7 +56,7 @@ class PageAccessPermission(db.Model):
     __tablename__ = "page_access_permission"
     permission_name = db.Column(db.String(100), primary_key=True)
 
-    users = db.relationship("GrantedPermission", back_populates="permission")
+    users = db.relationship("AllowedPermission", back_populates="permission")
 
     def __init__(self, permission_name):
         self.permission_name = permission_name
@@ -69,8 +69,8 @@ class User(db.Model):
     user_id = db.Column(db.String(100), primary_key=True)
     history = db.relationship("History", backref="owner")
 
-    projects = db.relationship("AvailableProject", back_populates="user",)
-    permissions = db.relationship("GrantedPermission", back_populates="user")
+    projects = db.relationship("AllowedProject", back_populates="user",)
+    permissions = db.relationship("AllowedPermission", back_populates="user")
 
     def __init__(self, user_id):
         self.user_id = user_id
