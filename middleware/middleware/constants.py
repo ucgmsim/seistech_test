797586a9601c381bdca917c7ae98f842a3627ede
sdw123123@gmail.com <sdw123123@gmail.com>
2021-03-16 12:19:08 +1300

    Adding constants for forwarding path

--- middleware/middleware/constants.py
+++ middleware/middleware/constants.py
@@ -8,16 +8,16 @@ CORE_API_STATION_ENDPOINT = "/coreAPI/station/get"
 
 # Seismic Hazard
 CORE_API_HAZARD_ENDPOINT = "/coreAPI/hazard/get"
-CORE_API_HAZARD_NZS1170P5_ENDPOINT = "/coreAPI/hazard/nz1170p5/get"
+CORE_API_HAZARD_NZS1170P5_ENDPOINT = "/coreAPI/hazard/nzs1170p5/get"
 CORE_API_HAZARD_NZS1170P5_SOIL_CLASS_ENDPOINT = (
-    "/coreAPI/hazard/nz1170p5/soil_class/get"
+    "/coreAPI/hazard/nzs1170p5/soil_class/get"
 )
 CORE_API_HAZARD_NZS1170P5_DEFAULT_PARAMS_ENDPOINT = (
-    "/coreAPI/hazard/nz1170p5/default/get"
+    "/coreAPI/hazard/nzs1170p5/default/get"
 )
 CORE_API_HAZARD_DISAGG_ENDPOINT = "/coreAPI/disagg/get"
 CORE_API_HAZARD_UHS_ENDPOINT = "/coreAPI/uhs/get"
-CORE_API_HAZARD_UHS_NZS1170P5_ENDPOINT = "/coreAPI/uhs/nz1170p5/get"
+CORE_API_HAZARD_UHS_NZS1170P5_ENDPOINT = "/coreAPI/uhs/nzs1170p5/get"
 
 # Ground Motion Selection
 CORE_API_GMS_ENDPOINT = "/coreAPI/gms/ensemble_gms/get"
@@ -78,3 +78,52 @@ INTERMEDIATE_API_ALL_PRIVATE_PROJECTS_ENDPOINT = (
 INTERMEDIATE_API_ALL_PUBLIC_PROJECTS_ENDPOINT = (
     "/intermediateAPI/project/public/get/all"
 )
+
+# Forwarding path to Core/Project API
+# GM data endpoints
+ENSEMBLE_IDS_ENDPOINT = "/api/gm_data/ensemble/ids/get"
+ENSEMBLE_IMS_ENDPOINT = "/api/gm_data/ensemble/ims/get"
+
+# Hazard endpoints
+ENSEMBLE_HAZARD_ENDPOINT = "/api/hazard/ensemble_hazard/get"
+ENSEMBLE_HAZARD_DOWNLOAD_ENDPOINT = "/api/hazard/ensemble_hazard/download"
+
+# NZS1170p5 endpoints
+NZS1170p5_HAZARD_ENDPOINT = "/api/hazard/nzs1170p5/get"
+NZS1170p5_UHS_ENDPOINT = "/api/uhs/nzs1170p5/get"
+NZS1170p5_DEFAULT_PARAMS_ENDPOINT = "/api/hazard/nzs1170p5/default_params"
+NZS1170p5_SOIL_CLASS = "/api/hazard/nzs1170p5/soil_class"
+
+# NZTA endpoints
+NZTA_HAZARD_ENDPOINT = "/api/hazard/nzta/get"
+NZTA_DEFAULT_PARAMS_ENDPOINT = "/api/hazard/nzta/default_params/get"
+NZTA_SOIL_CLASS = "/api/hazard/nzta/soil_class/get"
+
+# Disagg endpoints
+ENSEMBLE_DISAGG_ENDPOINT = "/api/disagg/ensemble_disagg/get"
+ENSEMBLE_DISAGG_DOWNLOAD_ENDPOINT = "/api/disagg/ensemble_disagg/download"
+ENSEMBLE_FULL_DISAGG_ENDPOINT = "/api/disagg/full_disagg/get"
+
+# UHS endpoints
+ENSEMBLE_UHS_ENDPOINT = "/api/uhs/ensemble_uhs/get"
+ENSEMBLE_UHS_DOWNLOAD_ENDPOINT = "/api/uhs/ensemble_uhs/download"
+
+# GMS endpoints
+ENSEMBLE_GMS_COMPUTE_ENDPOINT = "/api/gms/ensemble_gms/compute"
+ENSEMBLE_GMS_DOWNLOAD_ENDPOINT = "/api/gms/ensemble_gms/download"
+GMS_DEFAULT_IM_WEIGHTS_ENDPOINT = "/api/gms/ensemble_gms/get_default_IM_weights"
+GMS_IMS_ENDPOINT = "/api/gms/ensemble_gms/ims"
+GMS_DEFAULT_CAUSAL_PARAMS_ENDPOINT = "/api/gms/ensemble_gms/get_default_causal_params"
+GMS_GM_DATASETS_ENDPOINT = "/api/gms/ensemble_gms/datasets"
+
+# Rupture endpoints
+RUPTURES_ENDPOINT = "/api/rupture/ruptures/get"
+
+# Site endpoints
+SITE_LOCATION_ENDPOINT = "/api/site/station/location/get"
+SITE_NAME_ENDPOINT = "/api/site/station/name/get"
+SITE_CONTEXT_MAP_ENDPOINT = "/api/site/context/map/download"
+SITE_VS30_MAP_ENDPOINT = "/api/site/vs30/map/download"
+
+# Site-source endpoints
+SITE_SOURCE_DISTANCES_ENDPOINT = "/api/site_source/distances/get"
